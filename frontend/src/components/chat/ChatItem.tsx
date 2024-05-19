7650f78fb8a2a996bbb30df78fe64f1ab27011f6
Baba-Gandhi-Kandagatla <babagandhikandagatla@gmail.com>
2024-05-19 17:45:56 +0530

    Update ChatItem.tsx

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -2,10 +2,13 @@ import { Avatar, Box, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { HiSpeakerWave } from "react-icons/hi2";
+import { useState } from "react";
+
 
 function extractCodeFromString(message: string) {
-  if (message.includes("```")) {
-    const blocks = message.split("```");
+  if (message.includes("")) {
+    const blocks = message.split("");
     return blocks;
   }
 }
@@ -29,14 +32,37 @@ function determineLanguage(message: string) {
 const ChatItem = ({ content, role }: { content: string, role: "user" | "assistant" }) => {
   const auth = useAuth();
   const messageBlocks = extractCodeFromString(content);
-
+  
+  const [isClicked, setIsClicked] = useState(false);
+    
+  const handleClick = () => {
+      setIsClicked(!isClicked);
+  };
   return (
     role == "assistant"
       ? (
         <Box sx={{ display: 'flex', p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
-          <Avatar sx={{ ml: "0" }}>
-            <img src="kmitlogo.png" alt="kmit" width={"30px"} />
-          </Avatar>
+      <div
+        style={{
+          margin: 5,
+          padding: 5,
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+        }}
+      >
+        <Avatar sx={{ ml: "0" }}>
+          <img src="kmitlogo.png" alt="kmit" width={"30px"} />
+        </Avatar>
+        <div onClick={handleClick} style={{ padding: "5px" , backgroundColor: "rgba(150, 100, 200, 0.2)", outline: "transparent white 1px", borderRadius: "10px" }}>
+          {isClicked ? (
+            <HiSpeakerWave style={{ margin: 5, color: "blue", scale: 1.25 }} />
+          ) : (
+            <HiSpeakerWave style={{ margin: 5, color: "#004d56" }} />
+          )}
+        </div>
+      </div>
+
           <Box>
               {!messageBlocks && (
                 <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
