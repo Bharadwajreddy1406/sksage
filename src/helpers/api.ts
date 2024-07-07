dad1b3b50bf938244214bd6684e0b10783632678
Bharadwajreddy1406 <bharadwajreddy1406@gmail.com>
2024-07-07 13:56:17 +0530

    changed login api

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const loginUser = async (username:string, password:string)=>{
     
-    const res = await axios.post('/user/login', {username, password});
+    const res = await axios.post('/v1/user/login', {username, password});
     console.log("request sent")
     if(res.status != 200){
         throw new Error("unable to login")
